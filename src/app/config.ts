39cf4baee20119f3ff30b59434d1aa286623955d
Michael Hoe <michaelpaulhoe@gmail.com>
2019-05-17 14:42:52 +0100

    Flowershow panel created

--- src/app/config.ts
+++ src/app/config.ts
@@ -4,5 +4,10 @@ export class Config {
   flickrApiKey: string;
   flickrSecret: string;
   flickrGalleryId: string;
-  links: any;
+  flowerShow: flowerShow;
+}
+
+export interface flowerShow {
+  link: string;
+  date: string;
 }
