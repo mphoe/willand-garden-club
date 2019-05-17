1b8fb575fa182494df44ec4d03e2066c8d722ee1
Michael Hoe <michaelpaulhoe@gmail.com>
2019-05-17 16:58:01 +0100

    Events in the past display different

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { UpcomingEvent } from '../upcoming-event';
+import moment from 'moment';
 
 @Component({
   selector: 'app-event',
@@ -11,10 +12,17 @@ export class EventComponent implements OnInit {
 
   @Input() event: UpcomingEvent;
 
+  public inFuture: boolean;
+
 
   constructor() { }
 
   ngOnInit() {
+    this.isFutureEvent();
+  }
+
+  isFutureEvent() {
+    this.inFuture = moment(this.event.date, "DD/MM/YYYY").isAfter();
   }
 
 }
